018486e541b1e1610550d9e61f8719ef2a2485b7
Paoloesposito <espositoblsd@gmail.com>
2023-07-21 16:22:49 +0200

    .

--- smartphone.js
+++ smartphone.js
@@ -1,15 +1,35 @@
-// Classe firstUser implementa l'interfaccia Smartphone
-var FirstUser = /** @class */ (function () {
-    function FirstUser(nome) {
-        this.carica = 0;
-        this.numeroChiamate = 0;
-        this.registroChiamate = [];
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// Definizione dell'abstract class UtenteSmartphone
+var UtenteSmartphone = /** @class */ (function () {
+    function UtenteSmartphone(nome, carica, numeroChiamate, registroChiamate, immagine // Aggiungi il parametro immagine al costruttore
+    ) {
+        if (carica === void 0) { carica = 0; }
+        if (numeroChiamate === void 0) { numeroChiamate = 0; }
+        if (registroChiamate === void 0) { registroChiamate = []; }
         this.nome = nome;
+        this.carica = carica;
+        this.numeroChiamate = numeroChiamate;
+        this.registroChiamate = registroChiamate;
+        this.immagine = immagine;
     }
-    FirstUser.prototype.ricarica = function (unaRicarica) {
+    UtenteSmartphone.prototype.ricarica = function (unaRicarica) {
         this.carica += unaRicarica;
     };
-    FirstUser.prototype.chiamata = function (minutiDurata) {
+    UtenteSmartphone.prototype.chiamata = function (minutiDurata) {
         var costoChiamata = minutiDurata * 0.20;
         if (costoChiamata <= this.carica) {
             this.carica -= costoChiamata;
@@ -25,100 +45,41 @@ var FirstUser = /** @class */ (function () {
             console.log("Credito insufficiente per effettuare la chiamata.");
         }
     };
-    FirstUser.prototype.numero404 = function () {
+    UtenteSmartphone.prototype.numero404 = function () {
         return this.carica;
     };
-    FirstUser.prototype.getNumeroChiamate = function () {
+    UtenteSmartphone.prototype.getNumeroChiamate = function () {
         return this.numeroChiamate;
     };
-    FirstUser.prototype.azzeraChiamate = function () {
+    UtenteSmartphone.prototype.azzeraChiamate = function () {
         this.numeroChiamate = 0;
     };
-    return FirstUser;
+    return UtenteSmartphone;
 }());
-// Classe secondUser implementa l'interfaccia Smartphone
-var SecondUser = /** @class */ (function () {
-    function SecondUser(nome) {
-        this.carica = 0;
-        this.numeroChiamate = 0;
-        this.registroChiamate = [];
-        this.nome = nome;
-    }
-    SecondUser.prototype.ricarica = function (unaRicarica) {
-        this.carica += unaRicarica;
-    };
-    SecondUser.prototype.chiamata = function (minutiDurata) {
-        var costoChiamata = minutiDurata * 0.20;
-        if (costoChiamata <= this.carica) {
-            this.carica -= costoChiamata;
-            this.numeroChiamate++;
-            var nuovaChiamata = {
-                id: this.numeroChiamate,
-                durata: minutiDurata,
-                dataOra: new Date().toLocaleString()
-            };
-            this.registroChiamate.push(nuovaChiamata);
+// Creazione degli utenti
+var utenti = [
+    new /** @class */ (function (_super) {
+        __extends(class_1, _super);
+        function class_1() {
+            return _super.call(this, "Gandalf", 30, 2, [], "assets/gandalf.jpg") || this;
         }
-        else {
-            console.log("Credito insufficiente per effettuare la chiamata.");
+        return class_1;
+    }(UtenteSmartphone)),
+    new /** @class */ (function (_super) {
+        __extends(class_2, _super);
+        function class_2() {
+            return _super.call(this, "Sauron", 20, 1, [], "assets/sauron.jpg") || this;
         }
-    };
-    SecondUser.prototype.numero404 = function () {
-        return this.carica;
-    };
-    SecondUser.prototype.getNumeroChiamate = function () {
-        return this.numeroChiamate;
-    };
-    SecondUser.prototype.azzeraChiamate = function () {
-        this.numeroChiamate = 0;
-    };
-    return SecondUser;
-}());
-// Classe thirdUser implementa l'interfaccia Smartphone
-var ThirdUser = /** @class */ (function () {
-    function ThirdUser(nome) {
-        this.carica = 0;
-        this.numeroChiamate = 0;
-        this.registroChiamate = [];
-        this.nome = nome;
-    }
-    ThirdUser.prototype.ricarica = function (unaRicarica) {
-        this.carica += unaRicarica;
-    };
-    ThirdUser.prototype.chiamata = function (minutiDurata) {
-        var costoChiamata = minutiDurata * 0.20;
-        if (costoChiamata <= this.carica) {
-            this.carica -= costoChiamata;
-            this.numeroChiamate++;
-            var nuovaChiamata = {
-                id: this.numeroChiamate,
-                durata: minutiDurata,
-                dataOra: new Date().toLocaleString()
-            };
-            this.registroChiamate.push(nuovaChiamata);
+        return class_2;
+    }(UtenteSmartphone)),
+    new /** @class */ (function (_super) {
+        __extends(class_3, _super);
+        function class_3() {
+            return _super.call(this, "Frodo", 50, 3, [], "assets/frodo.jpg") || this;
         }
-        else {
-            console.log("Credito insufficiente per effettuare la chiamata.");
-        }
-    };
-    ThirdUser.prototype.numero404 = function () {
-        return this.carica;
-    };
-    ThirdUser.prototype.getNumeroChiamate = function () {
-        return this.numeroChiamate;
-    };
-    ThirdUser.prototype.azzeraChiamate = function () {
-        this.numeroChiamate = 0;
-    };
-    return ThirdUser;
-}());
-// Recupera i riferimenti agli elementi HTML in cui visualizzare i dati
-var user1DataElement = document.getElementById("user1Data");
-var user1CallLogElement = document.getElementById("user1CallLog");
-var user2DataElement = document.getElementById("user2Data");
-var user2CallLogElement = document.getElementById("user2CallLog");
-var user3DataElement = document.getElementById("user3Data");
-var user3CallLogElement = document.getElementById("user3CallLog");
+        return class_3;
+    }(UtenteSmartphone))
+];
 // Funzione per calcolare i minuti di chiamata possibili con il saldo residuo
 function calcolaMinutiPossibili(saldoResiduo) {
     var costoChiamataAlMinuto = 0.20;
@@ -129,27 +90,21 @@ function updateUserData(user, dataElement, callLogElement) {
     var saldoResiduo = user.numero404();
     var numeroChiamate = user.getNumeroChiamate();
     var minutiPossibili = calcolaMinutiPossibili(saldoResiduo);
-    dataElement.innerHTML = "\n    <h2>".concat(user.nome, "</h2>\n    <p>Saldo Residuo: ").concat(saldoResiduo, " Euro</p>\n    <p>Numero di Chiamate: ").concat(numeroChiamate, "</p>\n    <p>Minuti di Chiamata Possibili: ").concat(minutiPossibili, " minuti</p>\n  ");
+    dataElement.innerHTML = "\n    <h2>".concat(user.nome, "</h2>\n    <img src=\"").concat(user.immagine, "\" alt=\"").concat(user.nome, "\" width=\"100\">\n    <p>Saldo Residuo: ").concat(saldoResiduo, " Euro</p>\n    <p>Numero di Chiamate: ").concat(numeroChiamate, "</p>\n    <p>Minuti di Chiamata Possibili: ").concat(minutiPossibili, " minuti</p>\n  ");
     // Aggiungi il registro delle chiamate
     callLogElement.innerHTML = "";
     user.registroChiamate.forEach(function (chiamata) {
         callLogElement.innerHTML += "\n      <li>\n        ID: ".concat(chiamata.id, " | Durata: ").concat(chiamata.durata, " min | Data/Ora: ").concat(chiamata.dataOra, "\n      </li>\n    ");
     });
 }
-// Utilizzo delle classi per verificare il saldo residuo e il numero di chiamate per ogni utente
-var user1 = new FirstUser("Sauron");
-var user2 = new SecondUser("Gandalf");
-var user3 = new ThirdUser("Frodo");
-user1.ricarica(30);
-user1.chiamata(10);
-user1.chiamata(5);
-user2.ricarica(20);
-user2.chiamata(15);
-user3.ricarica(50);
-user3.chiamata(30);
-user3.chiamata(10);
-user3.chiamata(5);
-// Aggiorna i dati e il registro delle chiamate per ogni utente
-updateUserData(user1, user1DataElement, user1CallLogElement);
-updateUserData(user2, user2DataElement, user2CallLogElement);
-updateUserData(user3, user3DataElement, user3CallLogElement);
+// Aggiornamento dei dati e del registro delle chiamate per ogni utente
+utenti.forEach(function (utente, index) {
+    utente.ricarica(30 * (index + 1));
+    utente.chiamata(10 + (5 * index));
+    utente.chiamata(5 + (3 * index));
+    var userDataElement = document.getElementById("userData".concat(index + 1));
+    var userCallLogElement = document.getElementById("userCallLog".concat(index + 1));
+    if (userDataElement && userCallLogElement) {
+        updateUserData(utente, userDataElement, userCallLogElement);
+    }
+});
--- smartphone.ts
+++ smartphone.ts
@@ -1,78 +1,19 @@
-// Definizione dell'interfaccia Smartphone
-interface Smartphone {
-  nome: string;
-  carica: number;
-  numeroChiamate: number;
-  registroChiamate: Chiamata[];
-
-  ricarica(unaRicarica: number): void;
-  chiamata(minutiDurata: number): void;
-  numero404(): number;
-  getNumeroChiamate(): number;
-  azzeraChiamate(): void;
-}
-
-// Interfaccia per i dati di una chiamata
+// Definizione dell'interfaccia Chiamata
 interface Chiamata {
   id: number;
   durata: number;
   dataOra: string;
 }
 
-// Classe firstUser implementa l'interfaccia Smartphone
-class FirstUser implements Smartphone {
-  nome: string;
-  carica: number = 0;
-  numeroChiamate: number = 0;
-  registroChiamate: Chiamata[] = [];
-
-  constructor(nome: string) {
-    this.nome = nome;
-  }
-
-  ricarica(unaRicarica: number): void {
-    this.carica += unaRicarica;
-  }
-
-  chiamata(minutiDurata: number): void {
-    const costoChiamata = minutiDurata * 0.20;
-    if (costoChiamata <= this.carica) {
-      this.carica -= costoChiamata;
-      this.numeroChiamate++;
-      const nuovaChiamata: Chiamata = {
-        id: this.numeroChiamate,
-        durata: minutiDurata,
-        dataOra: new Date().toLocaleString()
-      };
-      this.registroChiamate.push(nuovaChiamata);
-    } else {
-      console.log("Credito insufficiente per effettuare la chiamata.");
-    }
-  }
-
-  numero404(): number {
-    return this.carica;
-  }
-
-  getNumeroChiamate(): number {
-    return this.numeroChiamate;
-  }
-
-  azzeraChiamate(): void {
-    this.numeroChiamate = 0;
-  }
-}
-
-// Classe secondUser implementa l'interfaccia Smartphone
-class SecondUser implements Smartphone {
-  nome: string;
-  carica: number = 0;
-  numeroChiamate: number = 0;
-  registroChiamate: Chiamata[] = [];
-
-  constructor(nome: string) {
-    this.nome = nome;
-  }
+// Definizione dell'abstract class UtenteSmartphone
+abstract class UtenteSmartphone {
+  constructor(
+    public nome: string,
+    public carica: number = 0,
+    public numeroChiamate: number = 0,
+    public registroChiamate: Chiamata[] = [],
+    public immagine: string // Aggiungi il parametro immagine al costruttore
+  ) {}
 
   ricarica(unaRicarica: number): void {
     this.carica += unaRicarica;
@@ -107,57 +48,24 @@ class SecondUser implements Smartphone {
   }
 }
 
-// Classe thirdUser implementa l'interfaccia Smartphone
-class ThirdUser implements Smartphone {
-  nome: string;
-  carica: number = 0;
-  numeroChiamate: number = 0;
-  registroChiamate: Chiamata[] = [];
-
-  constructor(nome: string) {
-    this.nome = nome;
-  }
-
-  ricarica(unaRicarica: number): void {
-    this.carica += unaRicarica;
-  }
-
-  chiamata(minutiDurata: number): void {
-    const costoChiamata = minutiDurata * 0.20;
-    if (costoChiamata <= this.carica) {
-      this.carica -= costoChiamata;
-      this.numeroChiamate++;
-      const nuovaChiamata: Chiamata = {
-        id: this.numeroChiamate,
-        durata: minutiDurata,
-        dataOra: new Date().toLocaleString()
-      };
-      this.registroChiamate.push(nuovaChiamata);
-    } else {
-      console.log("Credito insufficiente per effettuare la chiamata.");
+// Creazione degli utenti
+const utenti: UtenteSmartphone[] = [
+  new class extends UtenteSmartphone {
+    constructor() {
+      super("Gandalf", 30, 2, [], "assets/gandalf.jpg");
+    }
+  },
+  new class extends UtenteSmartphone {
+    constructor() {
+      super("Sauron", 20, 1, [], "assets/sauron.jpg");
+    }
+  },
+  new class extends UtenteSmartphone {
+    constructor() {
+      super("Frodo", 50, 3, [], "assets/frodo.jpg");
     }
   }
-
-  numero404(): number {
-    return this.carica;
-  }
-
-  getNumeroChiamate(): number {
-    return this.numeroChiamate;
-  }
-
-  azzeraChiamate(): void {
-    this.numeroChiamate = 0;
-  }
-}
-
-// Recupera i riferimenti agli elementi HTML in cui visualizzare i dati
-const user1DataElement = document.getElementById("user1Data");
-const user1CallLogElement = document.getElementById("user1CallLog");
-const user2DataElement = document.getElementById("user2Data");
-const user2CallLogElement = document.getElementById("user2CallLog");
-const user3DataElement = document.getElementById("user3Data");
-const user3CallLogElement = document.getElementById("user3CallLog");
+];
 
 // Funzione per calcolare i minuti di chiamata possibili con il saldo residuo
 function calcolaMinutiPossibili(saldoResiduo: number): number {
@@ -166,13 +74,14 @@ function calcolaMinutiPossibili(saldoResiduo: number): number {
 }
 
 // Funzione per aggiornare i dati dell'utente sulla pagina HTML
-function updateUserData(user: Smartphone, dataElement: HTMLElement, callLogElement: HTMLElement) {
+function updateUserData(user: UtenteSmartphone, dataElement: HTMLElement, callLogElement: HTMLElement) {
   const saldoResiduo = user.numero404();
   const numeroChiamate = user.getNumeroChiamate();
   const minutiPossibili = calcolaMinutiPossibili(saldoResiduo);
 
   dataElement.innerHTML = `
     <h2>${user.nome}</h2>
+    <img src="${user.immagine}" alt="${user.nome}" width="100">
     <p>Saldo Residuo: ${saldoResiduo} Euro</p>
     <p>Numero di Chiamate: ${numeroChiamate}</p>
     <p>Minuti di Chiamata Possibili: ${minutiPossibili} minuti</p>
@@ -189,24 +98,16 @@ function updateUserData(user: Smartphone, dataElement: HTMLElement, callLogEleme
   });
 }
 
-// Utilizzo delle classi per verificare il saldo residuo e il numero di chiamate per ogni utente
-const user1 = new FirstUser("Sauron");
-const user2 = new SecondUser("Gandalf");
-const user3 = new ThirdUser("Frodo");
+// Aggiornamento dei dati e del registro delle chiamate per ogni utente
+utenti.forEach((utente, index) => {
+  utente.ricarica(30 * (index + 1));
+  utente.chiamata(10 + (5 * index));
+  utente.chiamata(5 + (3 * index));
 
-user1.ricarica(30);
-user1.chiamata(10);
-user1.chiamata(5);
+  const userDataElement = document.getElementById(`userData${index + 1}`);
+  const userCallLogElement = document.getElementById(`userCallLog${index + 1}`);
 
-user2.ricarica(20);
-user2.chiamata(15);
-
-user3.ricarica(50);
-user3.chiamata(30);
-user3.chiamata(10);
-user3.chiamata(5);
-
-// Aggiorna i dati e il registro delle chiamate per ogni utente
-updateUserData(user1, user1DataElement, user1CallLogElement);
-updateUserData(user2, user2DataElement, user2CallLogElement);
-updateUserData(user3, user3DataElement, user3CallLogElement);
+  if (userDataElement && userCallLogElement) {
+    updateUserData(utente, userDataElement, userCallLogElement);
+  }
+});
